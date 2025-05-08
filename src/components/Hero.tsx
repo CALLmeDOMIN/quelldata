269d4b799f3d420d9f3961820a19e9b223a2afa9
Ignacy <bleklajtnik@gmail.com>
2025-05-08 17:03:32 +0200

    added hidden burger nav

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ import Navbar from '../components/Navbar';
 const Hero = () => {
   return (
     <section className="relative flex flex-col items-center justify-center">
-      <Navbar />
+      {/* <Navbar /> */}
       <div className="absolute inset-y-0 left-0 h-full w-px bg-neutral-200/80 dark:bg-neutral-800/80">
         <div className="absolute top-0 h-40 w-px bg-gradient-to-b from-transparent via-blue-500 to-transparent" />
       </div>
