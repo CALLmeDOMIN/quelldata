d852fd4635fcdbc21f822ab150d336547368a931
Ignacy <bleklajtnik@gmail.com>
2025-05-10 17:56:44 +0200

    mobile responsive form

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,17 +1,14 @@
-"use client";
-import { useState } from "react";
-import { motion } from "motion/react";
-import Navbar from "../components/Navbar";
-import SignupForm from "@/components/SignUpForm";
-
+'use client';
+import { useState } from 'react';
+import { motion } from 'motion/react';
+import Navbar from '../components/Navbar';
+import SignUpModal from './SignUpModal';
 const Hero = () => {
-  const [ isFormOpened, setIsFormOpened ] = useState(false)
-  function handleFormClick() {
-    setIsFormOpened(isFormOpened=>!isFormOpened);
-  }
+  const [isFormOpen, setIsFormOpen] = useState(false);
+
   return (
     <section className="relative flex flex-col items-center justify-center">
-      <Navbar />
+      {/* <Navbar /> */}
       <div className="absolute inset-y-0 left-0 h-full w-px bg-neutral-200/80 dark:bg-neutral-800/80">
         <div className="absolute top-0 h-40 w-px bg-gradient-to-b from-transparent via-blue-500 to-transparent" />
       </div>
@@ -23,23 +20,21 @@ const Hero = () => {
       </div>
       <div className="p-4">
         <h1 className="relative z-10 mx-auto max-w-4xl text-center text-2xl font-bold text-slate-700 text-4xl lg:text-7xl dark:text-slate-300">
-          {"Quelldata – on-demand high-quality datasets for your AI models "
-            .split(" ")
-            .map((word, index) => (
-              <motion.span
-                key={index}
-                initial={{ opacity: 0, filter: "blur(4px)", y: 10 }}
-                animate={{ opacity: 1, filter: "blur(0px)", y: 0 }}
-                transition={{
-                  duration: 0.3,
-                  delay: index * 0.1,
-                  ease: "easeInOut",
-                }}
-                className="mr-2 inline-block"
-              >
-                {word}
-              </motion.span>
-            ))}
+          {'Quelldata – on-demand high-quality datasets for your AI models '.split(' ').map((word, index) => (
+            <motion.span
+              key={index}
+              initial={{ opacity: 0, filter: 'blur(4px)', y: 10 }}
+              animate={{ opacity: 1, filter: 'blur(0px)', y: 0 }}
+              transition={{
+                duration: 0.3,
+                delay: index * 0.1,
+                ease: 'easeInOut',
+              }}
+              className="mr-2 inline-block"
+            >
+              {word}
+            </motion.span>
+          ))}
         </h1>
 
         <motion.p
@@ -55,8 +50,8 @@ const Hero = () => {
           }}
           className="relative z-10 mx-auto max-w-xl py-4 text-center text-lg font-normal text-neutral-600 dark:text-neutral-400"
         >
-          Get custom datasets sourced by a global community and validated with
-          transparency. Faster, cheaper, and tailored to your model’s needs.
+          Get custom datasets sourced by a global community and validated with transparency. Faster, cheaper, and
+          tailored to your model’s needs.
         </motion.p>
         <motion.div
           initial={{
@@ -71,16 +66,18 @@ const Hero = () => {
           }}
           className="relative z-10 mt-8 flex flex-wrap items-center justify-center gap-4"
         >
-          <button className="w-60 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200 cursor-pointer" onClick={handleFormClick}>
+          <button
+            className="w-60 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200 cursor-pointer"
+            onClick={() => setIsFormOpen(true)}
+          >
             Join the Whitelist
           </button>
-          
+
           <button className="w-60 transform rounded-lg border border-gray-300 bg-white px-6 py-2 font-medium text-black transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-100 dark:border-gray-700 dark:bg-black dark:text-white dark:hover:bg-gray-900 cursor-pointer">
             See Whitepaper
           </button>
-          
         </motion.div>
-        {isFormOpened && <SignupForm/>}
+        <SignUpModal isOpen={isFormOpen} onClose={() => setIsFormOpen(false)} />
         <motion.div
           initial={{
             opacity: 0,
