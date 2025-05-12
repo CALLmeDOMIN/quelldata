bc93684cc7a014d21e8f434147bf05d3c3df15d4
Dominik Sieroń <71605737+CALLmeDOMIN@users.noreply.github.com>
2025-05-12 10:19:23 +0200

    Added og-image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,21 @@ export const metadata: Metadata = {
         media: '(prefers-color-scheme: light)'
       }
     ]
+  },
+
+  openGraph: {
+    title: "Quelldata",
+    description:
+      "Decentralized data platform for AI. Order, validate & earn with crowdsourced, high-quality datasets. On-chain proof-of-quality.", // Use the same or a slightly different description for OG
+    url: "https://www.quelldata.com/",
+    images: [
+      {
+        url: 'https://www.quelldata.com/og-image.jpg',
+        width: 1177,
+        height: 681,
+        alt: 'Quelldata OG Image',
+      },
+    ],
   }
 };
 
