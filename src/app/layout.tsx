4403c60990f35a3c58ac5f61552e6007490c366a
Dominik Sieroń <71605737+CALLmeDOMIN@users.noreply.github.com>
2025-05-12 17:08:01 +0200

    Feat: Enable analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Footer from '@/components/Footer';
 import { Toaster } from 'sonner';
+import { Analytics } from '@vercel/analytics/next';
 // import { ReactScan } from "../components/ReactScan";
 
 const geistSans = Geist({
@@ -32,18 +33,18 @@ export const metadata: Metadata = {
   },
 
   openGraph: {
-    title: "Quelldata",
+    title: 'Quelldata',
     description:
-      "Decentralized data platform for AI. Order, validate & earn with crowdsourced, high-quality datasets. On-chain proof-of-quality.", // Use the same or a slightly different description for OG
-    url: "https://www.quelldata.com/",
+      'Decentralized data platform for AI. Order, validate & earn with crowdsourced, high-quality datasets. On-chain proof-of-quality.', // Use the same or a slightly different description for OG
+    url: 'https://www.quelldata.com/',
     images: [
       {
         url: 'https://www.quelldata.com/og-image.jpg',
         width: 1177,
         height: 681,
-        alt: 'Quelldata OG Image',
-      },
-    ],
+        alt: 'Quelldata OG Image'
+      }
+    ]
   }
 };
 
@@ -62,6 +63,7 @@ export default function RootLayout({
         {children}
         <Toaster position="top-center" />
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
